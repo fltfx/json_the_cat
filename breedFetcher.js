5078f4f3743c97ef6508fe15fdf5125c10bc82ac
C W <claudiaywong@msn.com>
2021-09-23 21:01:59 +0000

    added M+C testing via breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,32 @@
 const request = require('request');
-//const fs = require('fs');
 
-//intake only the necessary arguments
-let myArgs = process.argv.slice(2);
-let catTypeInput = myArgs[0];
+const fetchBreedDescription = function(breedName, callback) {
+  let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + catTypeInput;
+  request(URL, (error, response, body) => {
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body, typeof(body)); // Print the HTML for the Google homepage.
+  
+    //Edge Case: Request Failed
+    if (error !== null) {
+      callback(error, null); //return with auto-generated error message
+      process.exit(); //end script
+    }
+  
+    //Edge Case: Breed Not Found
+    if (body === "[]") {
+      //set the error to a custom message
+      error = "We couldn't find a breed that matches your search. Please check your spelling and try again.";
+      callback(error, null); //return custom error to index.js
+      process.exit(); //end script
+    }
+    
+    //if there are not any errors (that is, error=null in this case)
+    const data = JSON.parse(body);
+    //parse for description, and input it to cb to return to index.js
+    let catDescription = data[0].description;
+    callback(null, catDescription);
+  });
+};
 
-request(URL, (error, response, body) => {
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body, typeof(body)); // Print the HTML for the Google homepage.
-
-  //Edge Case: Request Failed
-  if (error !== null) {
-    console.log('error:', error); // Print the error if one occurred
-    process.exit();
-  }
-
-  //Edge Case: Breed Not Found
-  if (body === "[]") {
-    console.log("We couldn't find a breed that matches your search. Please check your spelling and try again.");
-    process.exit();
-  }
-
-  const data = JSON.parse(body);
-  //console.log(data);
-  //console.log(typeof data);
-
-  //print out description
-  let catDescription = data[0].description;
-  console.log(catDescription);
-});
-
+module.exports = { fetchBreedDescription };
